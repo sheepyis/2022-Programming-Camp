76820b7d0ceb8bfa8a5e72be3addba69be9a3e51
sheep_is <zx9086@naver.com>
2023-01-19 20:43:38 +0900

    test

--- test/js/share.js
+++ test/js/share.js
@@ -1,6 +1,6 @@
 //페이스북 공유
 function shareFacebook() {
-    var sendUrl = "https://sheepyis.github.io/Programming-Camp/test/language.html"; // 전달할 URL
+    var sendUrl = "https://sheepyis.github.io/2022-Programming-Camp/test/language.html"; // 전달할 URL
     window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
 }
 
@@ -13,16 +13,16 @@ function sendLink() {
             description: '#2023 #한성대 #컴퓨터공학부 #동계 프로그래밍 캠프',
             imageUrl: './image/main.png',
             link: {
-                mobileWebUrl: 'https://sheepyis.github.io/Programming-Camp/test/language.html',
-                webUrl: 'https://sheepyis.github.io/Programming-Camp/test/language.html',
+                mobileWebUrl: 'https://sheepyis.github.io/2022-Programming-Camp/test/language.html',
+                webUrl: 'https://sheepyis.github.io/2022-Programming-Camp/test/language.html',
             },
         },
         buttons: [
             {
                 title: '프로그래밍 언어 만나러 가기',
                 link: {
-                    mobileWebUrl: 'https://sheepyis.github.io/Programming-Camp/test/language.html',
-                    webUrl: 'https://sheepyis.github.io/Programming-Camp/test/language.html',
+                    mobileWebUrl: 'https://sheepyis.github.io/2022-Programming-Camp/test/language.html',
+                    webUrl: 'https://sheepyis.github.io/2022-Programming-Camp/test/language.html',
                 },
             },
         ],
@@ -32,7 +32,7 @@ function sendLink() {
 //클립보드에 복사
 function clip() {
 
-    var url = 'https://sheepyis.github.io/Programming-Camp/test/language.html';
+    var url = 'https://sheepyis.github.io/2022-Programming-Camp/test/language.html';
     var textarea = document.createElement("textarea");
     document.body.appendChild(textarea);
     textarea.value = url;
